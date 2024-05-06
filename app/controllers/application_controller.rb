58c21f7ec87a4710309dc5a626ec2402a5b6f663
Adomas Auga <35742185+aauga@users.noreply.github.com>
2024-05-06 20:49:25 +0300

    Configure HTTP response bodies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@
 class ApplicationController < ActionController::API
   attr_reader :user
 
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :bad_request
+
   def authenticate
     @user ||= User.find(session[:user_id])
   rescue StandardError
@@ -13,5 +16,23 @@ class ApplicationController < ActionController::API
     return unless session[:user_id]
 
     render status: :forbidden
+  end
+
+  def internal_server_error(error)
+    raise error unless Rails.env.production?
+
+    error_response('System Error', :internal_server_error)
+  end
+
+  def not_found
+    error_response('Not Found', :not_found)
+  end
+
+  def bad_request(error)
+    error_response(error.message, :bad_request)
+  end
+
+  def error_response(message, status)
+    render json: { error_message: message }, status:
   end
 end
