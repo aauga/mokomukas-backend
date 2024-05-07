171b3a0aae7f4bd69fed343066a3a4505cfbd65a
Adomas Auga <35742185+aauga@users.noreply.github.com>
2024-05-07 14:07:17 +0300

    fix current_user method in controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,8 @@ class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordInvalid, with: :bad_request
 
   def current_user
+    return unless session[:user_id]
+
     User.find(session[:user_id])
   end
 
